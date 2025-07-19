56e3fd01065267f7cc7eea4dd1a9a9bff3da8cfd
nico-senac <nicolas.maguilar@senacsp.edu.br>
2025-07-18 21:42:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send(\`
+    res.send(`
         <h1>Vulnerable Node.js App</h1>
         <form method="POST" action="/eval">
             <label>Enter JavaScript code:</label><br>
             <input type="text" name="code" style="width:400px" />
             <button type="submit">Execute</button>
         </form>
-    \`);
+    `);
 });
 
 app.post('/eval', (req, res) => {
     const code = req.body.code;
     try {
         const result = eval(code); // Vulnerable to RCE
-        res.send(\`<pre>Result: \${result}</pre>\`);
+        res.send(`<pre>Result: \${result}</pre>`);
     } catch (err) {
-        res.send(\`<pre>Error: \${err.message}</pre>\`);
+        res.send(`<pre>Error: \${err.message}</pre>`);
     }
 });
 
 app.listen(port, () => {
-    console.log(\`Vulnerable app listening at http://localhost:\${port}\`);
+    console.log(`Vulnerable app listening at http://localhost:\${port}`);
 });
